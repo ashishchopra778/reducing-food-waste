3d48f03db47a6fae166290b4de628feb8639cfcf
Bibhas <sbibhas2008@gmail.com>
2020-04-11 13:03:51 +1000

    Dashboard grid

--- frontend/src/components/pages/DashAlerts.js
+++ frontend/src/components/pages/DashAlerts.js
@@ -4,13 +4,47 @@ import Header from './../layout/Header'
 
 function DashAlerts() {
   return (
-    <div>
+    <div className="Dash-bg">
       <Header/>
-      <div style={{textAlign: "center"}}>
-        <h1>Hello This is the Dash Alerts page</h1>
+      <div class="container-fluid pt-5 h-100">
+        <div class="container-fluid pt-3 h-100">
+        <div class="row h-100">
+          <div class="col-2 border border-light h-100">
+            <div class="element">NAV</div>
+          </div>
+          <div class="col-10">
+            <div class="container h-75 mt-5">
+            <div class="row h-50">
+              <div class="col-12">
+                <div class="row h-100">
+                  <div class="col-6"> 
+                    <div class="container border border-light h-100"> Element</div> 
+                  </div>
+                  <div class="col-6">
+                    <div class="container border border-light h-100"> Element</div>  
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="row h-50 mt-4">
+              <div class="col-12">
+                <div class="row h-100">
+                  <div class="col-6"> 
+                      <div class="container border border-light h-100"> Element</div> 
+                  </div>
+                  <div class="col-6">
+                    <div class="container border border-light h-100"> Element</div>  
+                  </div>
+                  </div>
+                </div>
+            </div>
+            </div>
+          </div>
+          
+        </div>
+        </div>
       </div>
-    </div>
-    
+  </div> 
   )
 }
 
