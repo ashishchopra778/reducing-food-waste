0f5cb7a14ba3b06071836f483571d40054b87429
Bibhas <sbibhas2008@gmail.com>
2020-04-12 15:55:47 +1000

    Dashboard grid

--- frontend/src/components/pages/DashAlerts.js
+++ frontend/src/components/pages/DashAlerts.js
@@ -7,10 +7,9 @@ function DashAlerts() {
     <div className="Dash-bg">
       <Header/>
       <div class="container-fluid pt-5 h-100">
-        <div class="container-fluid pt-3 h-100">
-        <div class="row h-100">
-          <div class="col-2 border border-light h-100">
-            <div class="element">NAV</div>
+        <div class="row h-100 pt-4">
+          <div class="col-2 h-100 shadow-lg p-5 bg-white border border-light">
+            <div class="container-fluid mt-5">NAV</div>
           </div>
           <div class="col-10">
             <div class="container h-75 mt-5">
@@ -18,10 +17,10 @@ function DashAlerts() {
               <div class="col-12">
                 <div class="row h-100">
                   <div class="col-6"> 
-                    <div class="container border border-light h-100"> Element</div> 
+                    <div class="container h-100 bg-white shadow p-5 rounded"> Element</div> 
                   </div>
                   <div class="col-6">
-                    <div class="container border border-light h-100"> Element</div>  
+                    <div class="container h-100 bg-white shadow p-5 rounded"> Element</div>  
                   </div>
                 </div>
               </div>
@@ -30,18 +29,16 @@ function DashAlerts() {
               <div class="col-12">
                 <div class="row h-100">
                   <div class="col-6"> 
-                      <div class="container border border-light h-100"> Element</div> 
+                      <div class="container h-100 bg-white shadow p-5 rounded"> Element</div> 
                   </div>
                   <div class="col-6">
-                    <div class="container border border-light h-100"> Element</div>  
+                    <div class="container h-100 bg-white shadow p-5 rounded"> Element</div>  
                   </div>
                   </div>
                 </div>
             </div>
             </div>
           </div>
-          
-        </div>
         </div>
       </div>
   </div> 
