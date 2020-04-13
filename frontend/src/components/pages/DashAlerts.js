0024525cbb8b70fa551f5bfb86c479762e8bf6a5
Bibhas <sbibhas2008@gmail.com>
2020-04-13 22:33:46 +1000

    Embedded powe bi

--- frontend/src/components/pages/DashAlerts.js
+++ frontend/src/components/pages/DashAlerts.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import Header from './../layout/Header'
 
+
 function DashAlerts() {
   return (
     <div className="Dash-bg">
@@ -17,10 +18,17 @@ function DashAlerts() {
               <div class="col-12">
                 <div class="row h-100">
                   <div class="col-6"> 
-                    <div class="container h-100 bg-white shadow p-5 rounded"> Element</div> 
+                    <div class="container h-100 bg-white shadow rounded"> 
+                      <div class="embed-responsive embed-responsive-16by9">
+                        <iframe width="1140" height="541.25" src="https://app.powerbi.com/reportEmbed?reportId=0927d59e-54ed-47a6-a49b-2f86af043148&groupId=4001c553-d51b-4145-82b4-b02d62bc38ba&autoAuth=true&ctid=3ff6cfa4-e715-48db-b8e1-0867b9f9fba3&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly93YWJpLWF1c3RyYWxpYS1zb3V0aGVhc3QtcmVkaXJlY3QuYW5hbHlzaXMud2luZG93cy5uZXQvIn0%3D" frameborder="0" allowFullScreen="true"></iframe>
+                      </div>
+                    </div> 
                   </div>
                   <div class="col-6">
-                    <div class="container h-100 bg-white shadow p-5 rounded"> Element</div>  
+                    <div class="container h-100 bg-white shadow rounded"> 
+                      <div class="embed-responsive embed-responsive-16by9">
+                        <iframe width="1140" height="541.25" src="https://app.powerbi.com/reportEmbed?reportId=b2c7f30f-3343-47fe-a959-efe773743d0d&groupId=4001c553-d51b-4145-82b4-b02d62bc38ba&autoAuth=true&ctid=3ff6cfa4-e715-48db-b8e1-0867b9f9fba3&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly93YWJpLWF1c3RyYWxpYS1zb3V0aGVhc3QtcmVkaXJlY3QuYW5hbHlzaXMud2luZG93cy5uZXQvIn0%3D" frameborder="0" allowFullScreen="true"></iframe>                      </div>
+                    </div>  
                   </div>
                 </div>
               </div>
