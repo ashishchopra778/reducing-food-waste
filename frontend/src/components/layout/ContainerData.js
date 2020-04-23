1f0bca617a2cb9439ae7db1539383cb0a680d791
Bibhas <sbibhas2008@gmail.com>
2020-04-23 16:07:03 +1000

    final dash

--- frontend/src/components/layout/ContainerData.js
+++ frontend/src/components/layout/ContainerData.js
@@ -7,11 +7,11 @@ function ContainerData () {
         <div class="col-12">
           <div class="container h-100 bg-white shadow rounded"> 
             <div class="embed-responsive embed-responsive-16by9">
-              <iframe width="1140" height="541.25" src="https://app.powerbi.com/reportEmbed?reportId=2b2f8024-f449-41bc-a973-8c83cf43fdca&autoAuth=true&ctid=3ff6cfa4-e715-48db-b8e1-0867b9f9fba3&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly93YWJpLWF1c3RyYWxpYS1zb3V0aGVhc3QtcmVkaXJlY3QuYW5hbHlzaXMud2luZG93cy5uZXQvIn0%3D" frameborder="0" allowFullScreen="true"></iframe>
-            </div>
-          </div>       
+              <iframe width="1140" height="541.25" src="https://app.powerbi.com/reportEmbed?reportId=3853d1fc-709a-4e48-9b30-f9793d2995b6&autoAuth=true&ctid=3ff6cfa4-e715-48db-b8e1-0867b9f9fba3&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly93YWJpLWF1c3RyYWxpYS1zb3V0aGVhc3QtcmVkaXJlY3QuYW5hbHlzaXMud2luZG93cy5uZXQvIn0%3D" frameborder="0" allowFullScreen="true"></iframe>        
+            </div>       
         </div>
       </div>
+    </div>
     </div>
   )    
 }
